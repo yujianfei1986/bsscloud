8dd2c48e03a26eea8991bf41acf30ff541c1980f
Admin <daredevil2@126.com>
2019-08-19 17:25:29 +0800

    单元测试变更->注入@Controller

--- src/test/java/com/bss/controller/LoginControllerTest.java
+++ src/test/java/com/bss/controller/LoginControllerTest.java
@@ -3,20 +3,31 @@ package com.bss.controller;
 import com.bss.entity.User;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.ModelMap;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Admin on 2019/8/19.
  * 登陆测试
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/config/spring-servlet.xml"})
 public class LoginControllerTest {
+
+    @Resource
+    private LoginController loginController;
 
     @Test
     public void testLogin() {
         User user = new User();
         user.setUser_name("");
         user.setUser_passwd("yjf1986");
-        LoginController loginController = new LoginController();
         String res = loginController.loginAction(user, new ModelMap());
         assertEquals("../../login", res);
     }
